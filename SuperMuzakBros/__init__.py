41b4c836c617cb699e3c2267e0509ec9179416fd
muzak23 <muzak23.teen@gmail.com>
2025-09-07 13:31:17 -0500

    Experimental cache busting for static files

--- SuperMuzakBros/__init__.py
+++ SuperMuzakBros/__init__.py
@@ -1,7 +1,8 @@
+import hashlib
 import os
 from dotenv import load_dotenv
 
-from flask import Flask
+from flask import Flask, url_for
 from flask_socketio import SocketIO
 
 
@@ -14,6 +15,23 @@ def create_app():
     if os.getenv('SECRET_KEY') is None:
         raise ValueError('SECRET_KEY is not set in .env')
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+    def build_static_map():
+        cache = {}
+        for name in ('game.js', 'game.css'):
+            path = os.path.join(app.static_folder, name)
+            with open(path, 'rb') as f:
+                cache[name] = hashlib.md5(f.read()).hexdigest()[:8]
+        return cache
+
+    STATIC_HASHES = build_static_map()
+
+    @app.context_processor
+    def inject_static():
+        def static_file(filename):
+            h = STATIC_HASHES.get(filename, '0')
+            return url_for('static', filename=filename, v=h)
+        return dict(static_file=static_file)
 
     with app.app_context():
         # blueprint for main game page/APIs
